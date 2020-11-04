c1c54e96ecf787a978cf0fe9cdd2f74b8eea53b3
lea soussan <leasoussan@hmail.com>
2020-11-04 22:32:17 +0200

    users first set up

---
+++ users.js
@@ -0,0 +1,47 @@
+function addUser(event){
+  event.preventDefault()
+        
+  let info = document.getElementById("myform")
+  let username = document.getElementById("username") 
+  let password = document.getElementById("password")
+  let programm = document.getElementById("weeks")  
+
+  addUser.username = username;
+  addUser.password = password;
+  addUser.programm = programm;
+    
+}
+
+
+
+
+// let addUserbtn = document.querySelector("button")
+
+// addUserbtn.addEventListener("click", addUser)
+
+
+// Initialize an object with properties and methods
+const usersDatabase = [{
+    username: "lola",
+    password: "123456",
+    programm: "12 weeks"
+  },
+  
+  {
+    username: "jean",
+    password: "09876",
+    programm: "10 weeks",
+  },
+
+  {
+    username: "Doris",
+    password: "11223344",
+    programm: "12 weeks",
+  },
+  {
+      username: "miro",
+      password: "24680",
+      programm: "14 weeeks",
+  }
+]
+
