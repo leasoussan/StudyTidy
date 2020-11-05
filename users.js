64056eb80fec8d3b5b56e5e74b262be592697b98
lea soussan <leasoussan@hmail.com>
2020-11-05 11:40:28 +0200

    Users login works

--- users.js
+++ users.js
@@ -1,58 +1,123 @@
-function addUser(event){
-  event.preventDefault()
 
-        
-  let info = document.getElementById("myform")
-  let username = document.getElementById("username").value 
-  let password = document.getElementById("password").value
-  let programm = document.getElementById("program").value
-  
-  console.log(username)
-  console.log(password)
-  console.log(programm)
-  
-  
-
-
-
-
-let addUserbtn = document.querySelector("button")
-
-addUserbtn.addEventListener("click", addUser)
-
-
-function user (fname, lname) {
-  this.usertName = fname;
-  this.password = password;
-  this.programm = programm;
-
-}
-
-
-
-
-// Initialize an object with properties and methods
 let usersDatabase = {
-    "username": "lola",
-    "password": "123456",
-    "programm": "12 weeks"
-  },
-  
-  {
-    username: "jean",
-    password: "09876",
-    programm: "10 weeks"
-  },
 
-  {
+      lola: {
+      username: "lola",
+      password: "123456"
+      
+      },
+  
+      jean: {
+      username: "jean",
+      password: "09876"
+      },
+
+      doris: {
     username: "Doris",
-    password: "11223344",
-    programm: "12 weeks"
-  },
-  {
+    password: "11223344"
+      },
+      miro: {
       username: "miro",
-      password: "24680",
-      programm: "14 weeeks"
+      password: "24680"
+     }
+    }
+
+// console.log(usersDatabase)
+
+let loginButton = document.getElementById("button")
+
+
+
+
+loginButton.addEventListener("click", logIn)
+
+function logIn(event){
+  event.preventDefault()
+  // alert("you try to log in")
+  let userName = document.getElementById("username").value
+  let passWord = document.getElementById("password").value
+
+  // usersDatabase[username]
+//beacuse we are in an object = we can't loop so we give conditional
+  if (typeof (userName) == "undefined"){
+    alert("you are not registered")
+  } else {
+    if (usersDatabase[userName]["password"] == passWord){
+      location.href = "index.html"
+    }
   }
 
+  }
+
+
+//   for (i = 0; i<usersDatabase.length; i++){
+//     if( usersDatabase[i].username === userName){
+//       alert("Welcome")
+    
+//       location.href =  "/file:///C:/Users/Lea%20Soussan/Desktop/StudyTidy/index.html"
+//       break;
+//     }
+//     else {
+//       alert("please register before")
+//       break;
+//     }
+//   }
+// }
+
+
+
+
+
+// let topicsObject = {
+//   "weekContainer-1": {
+//       "topicContainer-1-1": {
+//           "topicName": "HTML Basics",
+//           "exercises": {
+//               "Practice": "green",
+//               "XP": "red",
+//               "XP Gold": "yellow",
+//               "XP Ninja": "orange",
+//               "Mini-project": "green"
+//           },
+//           "notes": "some notes",
+//           "url": "www.google.com"
+//       },
+//       "topicContainer-1-2": {
+//           "topicName": "CSS Basics",
+//           "exercises": {
+//               "Practice": "red",
+//               "XP": "green",
+//               "Mini-project": "yellow"
+//           },
+//           "notes": "some notes",
+//           "url": "www.google.com"
+//       }
+//   },
+//   "weekContainer-2": {
+//       "topicContainer-2-1": {
+//           "topicName": "JavaScript Basics",
+//           "exercises": {
+//               "Practice": "red",
+//               "XP": "green",
+//               "XP Gold": "yellow",
+//               "XP Ninja": "orange",
+//               "Mini-project": "red"
+//           },
+//           "notes": "some notes",
+//           "url": "www.google.com"
+//       },
+//       "topicContainer-2-2": {
+//           "topicName": "JavaScript Basics",
+//           "exercises": {
+//               "Practice": "red",
+//               "XP": "green",
+//               "XP Gold": "yellow",
+//               "XP Ninja": "orange",
+//               "Mini-project": "red"
+//           },
+//           "notes": "some notes",
+//           "url": "www.google.com"
+//       }
+//   }
+// }
 
